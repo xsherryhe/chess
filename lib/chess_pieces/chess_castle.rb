f8386d20af95a81fc6717a7ca6f9e96e8cf6db38
Sherry He <xsherryhe@gmail.com>
2022-06-06 15:03:06 -0400

    Separated castle methods into their own module

---
+++ lib/chess_pieces/chess_castle.rb
@@ -0,0 +1,27 @@
+module Castle
+  private
+
+  def castle_instruction
+    'Castling is also available for this rook. ' \
+    'Please enter the word "castle" to make a castling move.'
+  end
+
+  def move_with_castle_instruction
+    move_instruction + "\r\n" + castle_instruction
+  end
+
+  def error_with_castle_message
+    error_message + "\r\n" + castle_instruction
+  end
+
+  def valid_castle_input(board, move_num)
+    puts move_with_castle_instruction
+    input = gets.chomp
+    until input =~ /^castle$/i ||
+          legal_next_positions(board, move_num).include?(to_pos(input))
+      puts error_with_castle_message
+      input = gets.chomp
+    end
+    input
+  end
+end
--- lib/chess_pieces/chess_rook.rb
+++ lib/chess_pieces/chess_rook.rb
@@ -1,6 +1,8 @@
 require_relative './chess_piece.rb'
+require_relative './chess_castle.rb'
 
 class Rook < Piece
+  include Castle
   attr_reader :moved
 
   def initialize(player_index, starting_position)
@@ -30,30 +32,6 @@ class Rook < Piece
     input = valid_castle_input(board, move_num)
     return @position = to_pos(input) unless input =~ /^castle$/i
 
-    king.castle(self, board)
-  end
-
-  def castle_instruction
-    'Castling is also available for this rook. ' \
-    'Please enter the word "castle" to make a castling move.'
-  end
-
-  def move_with_castle_instruction
-    move_instruction + "\r\n" + castle_instruction
-  end
-
-  def error_with_castle_message
-    error_message + "\r\n" + castle_instruction
-  end
-
-  def valid_castle_input(board, move_num)
-    puts move_with_castle_instruction
-    input = gets.chomp
-    until input =~ /^castle$/i ||
-          legal_next_positions(board, move_num).include?(to_pos(input))
-      puts error_with_castle_message
-      input = gets.chomp
-    end
-    input
+    king.castle(self)
   end
 end
